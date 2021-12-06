c2b2e839ed1dce5d227ac5bb500e90e8eebc32e8
Stefan Brandl <steve260897@gmail.com>
2021-12-06 12:43:16 +0100

    Optimized that shit

--- 06/06.py
+++ 06/06.py
@@ -1,23 +1,18 @@
+from collections import Counter, deque
+
 if __name__ == '__main__':
-    def simulate_pop(pop: dict[int, int], days: int):
+    def simulate_pop(pop: deque, days: int):
+        pop_iter = pop.copy()
         for day in range(days):
-            pop = {
-                0: pop[1],
-                1: pop[2],
-                2: pop[3],
-                3: pop[4],
-                4: pop[5],
-                5: pop[6],
-                6: pop[7] + pop[0],
-                7: pop[8],
-                8: pop[0],
-            }
-        return sum(pop.values()), pop
+            p = pop_iter.popleft()
+            pop_iter[6] += p
+            pop_iter.append(p)
+        return sum(pop_iter), pop_iter
 
     with open('input.txt') as in_file:
-        data = list(map(int, in_file.read().strip().split(',')))
+        counts = Counter(map(int, in_file.read().strip().split(',')))
 
-    pop0 = {i: data.count(i) for i in range(9)}
+    pop0 = deque([counts[i] for i in range(9)])
     part1, pop80 = simulate_pop(pop0, days=80)
     part2, pop256 = simulate_pop(pop80, days=256 - 80)
     print(part1, part2, sep='\n')
